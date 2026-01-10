76338f62d218c27d1f066e4747a1a54d28b74e07
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-01-10 09:56:37 +0700

    feat: register new export

--- packages/serv/tsup.config.ts
+++ packages/serv/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts'],
+	entry: ['src/index.ts', 'src/exports/orpc.ts'],
 	format: ['cjs', 'esm'],
 	dts: true,
 	splitting: false,
