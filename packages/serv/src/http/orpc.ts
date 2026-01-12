6070e13247701597d38f585fe3898662c97d726e
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-01-12 18:32:15 +0700

    feat: allow sending function

--- packages/serv/src/http/orpc.ts
+++ packages/serv/src/http/orpc.ts
@@ -9,16 +9,22 @@ type MaybeOptionalOptions<TOptions> =
 		? [options?: TOptions]
 		: [options: TOptions];
 
+type PossibleOpt<T extends Context, E, R> =
+	| FriendlyStandardHandleOptions<T>
+	| Effect.Effect<FriendlyStandardHandleOptions<T>, E, R>;
+
+function getValue<T extends Context, E, R>(opt: PossibleOpt<T, E, R>) {
+	return Effect.isEffect(opt) ? opt : Effect.succeed(opt);
+}
+
 export function oRPCHandler<T extends Context, E, R>(
 	handler: FetchHandler<T>,
-	opt?:
-		| FriendlyStandardHandleOptions<T>
-		| Effect.Effect<FriendlyStandardHandleOptions<T>, E, R>,
+	opt?: PossibleOpt<T, E, R> | (() => PossibleOpt<T, E, R>),
 ) {
 	return new Handler('oRPCHandler', ({ request }) =>
 		Effect.gen(function* () {
 			const _opt = (
-				opt ? [Effect.isEffect(opt) ? yield* opt : opt] : []
+				opt ? [yield* getValue(typeof opt === 'function' ? opt() : opt)] : []
 			) as MaybeOptionalOptions<FriendlyStandardHandleOptions<T>>;
 			return yield* Effect.tryPromise(() => handler.handle(request, ..._opt));
 		}),
