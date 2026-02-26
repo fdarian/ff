576c0c606ced4f100f135a7e0badafbd50d4c2e3
Farrel Darian <62016900+fdarian@users.noreply.github.com>
2026-02-26 11:44:34 +0700

    test: e2e

---
+++ packages/effect/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          include: ['src/**/*.test.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'e2e',
+          include: ['e2e/**/*.test.ts'],
+        },
+      },
+    ],
+  },
+})
